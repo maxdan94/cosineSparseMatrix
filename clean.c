85ce14ec9009db4701a1cd9449841870c4df8916
loycckvqtd <maximilien.danisch@telecom-paristech.fr>
2018-02-23 15:50:29 +0100

    better cleaning

--- clean.c
+++ clean.c
@@ -1,6 +1,6 @@
 /*
 gcc clean.c -O9 -o clean
-./clean max_in-degree net_input net_output
+./clean max_in-degree max_out-degree net_input net_output
 */
 
 #include <stdlib.h>
@@ -16,30 +16,33 @@ unsigned max3(unsigned a,unsigned b,unsigned c){
 }
 
 int main(int argc,char** argv){
-	unsigned s,t,n=0,n_max=NNODES,max=atoi(argv[1]);
-	unsigned *d=malloc(n_max*sizeof(unsigned));
+	unsigned s,t,n=0,n_max=NNODES,maxi=atoi(argv[1]),maxo=atoi(argv[2]);
+	unsigned *d_i=malloc(n_max*sizeof(unsigned)),*d_o=malloc(n_max*sizeof(unsigned));
 	FILE *file1,*file2;
 
 	printf("Maximum in-degree allowed = %u\n",max);
 
-	printf("Reading edgelist from file %s\n",argv[2]);
-	file1=fopen(argv[1],"r");
+	printf("Reading edgelist from file %s\n",argv[3]);
+	file1=fopen(argv[3],"r");
 	while (fscanf(file1,"%u %u\n", &s, &t)==2) {
 		n=max3(n,s+1,t+1);
 		if (n>n_max) {
-			d=realloc(d,(n+NNODES)*sizeof(unsigned));
-			bzero(d+n_max,(n+NNODES-n_max)*sizeof(unsigned));
+			d_i=realloc(d_i,(n+NNODES)*sizeof(unsigned));
+			bzero(d_i+n_max,(n+NNODES-n_max)*sizeof(unsigned));
+			d_o=realloc(d_o,(n+NNODES)*sizeof(unsigned));
+			bzero(d_o+n_max,(n+NNODES-n_max)*sizeof(unsigned));
 			n_max=n+NNODES;
 		}
-		d[t]++;
+		d_i[t]++;
+		d_o[s]++;
 	}
 	fclose(file1);
 
 	printf("Writting edgelist in file %s\n",argv[3]);
-	file1=fopen(argv[1],"r");
-	file2=fopen(argv[2],"w");
+	file1=fopen(argv[3],"r");
+	file2=fopen(argv[4],"w");
 	while (fscanf(file1,"%u %u\n", &s, &t)==2) {
-		if (d[t]<max){
+		if (d[s]<maxo && d[t]<maxi){
 			fprintf(file2,"%u %u\n", s, t);
 		}
 	}
